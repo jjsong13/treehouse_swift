3799a7be4a6a3f92120e6fcf5fabb6e177d5e30e
Jamie <jsj.jamie@gmail.com>
2016-06-23 23:52:40 +0900

    Treehouse: Swift tutorial

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello"
+
+let modernProgrammingLanguage = "Swift"
+
+var greeting = "\(str) \(modernProgrammingLanguage)"
+
+println("To build an iPhone app we need to learn \(modernProgrammingLanguage)")
+
+println("\n List: \n 1 - 🍅 \n 2 - 🍌 \n 6 - 🍊")
+
+let language : String = "Swift"
+
+let year : Int = 2014 //Int or Integer
+
+var version : Double = 1.0 //Double = 15 decimal and Float = 7 decimal
+
+var isAwesome : Bool = true //Boolean or Bool
+
+
